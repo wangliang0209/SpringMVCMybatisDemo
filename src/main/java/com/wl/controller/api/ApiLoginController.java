3677f7e6ef9d9e5db925f34563386e3cd65db69a
wangliang <wangliang0209@163.com>
2016-11-07 22:38:09 +0800

    wangliang:modify target project name.
    Signed-off-by: wangliang <wangliang0209@163.com>

--- src/main/java/com/wl/controller/api/ApiLoginController.java
+++ src/main/java/com/wl/controller/api/ApiLoginController.java
@@ -1,29 +1,21 @@
 package com.wl.controller.api;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.UUID;
-
-import org.apache.tomcat.util.security.MD5Encoder;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.wl.model.Account;
+import com.wl.service.RegService;
+import com.wl.service.TokenService;
+import com.wl.web.util.MD5;
+import com.wl.web.util.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.mysql.fabric.xmlrpc.base.Data;
-import com.wl.authorization.annotation.Authorization;
-import com.wl.authorization.manager.impl.RedisTokenManager;
-import com.wl.model.Account;
-import com.wl.service.RegService;
-import com.wl.service.TokenService;
-import com.wl.web.util.MD5;
-import com.wl.web.util.ResponseUtils;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * 登陆控制器
